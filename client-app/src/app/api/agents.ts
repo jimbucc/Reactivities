0534c74a63e76b1c36456f7440f366c7c17f426f
James Bucciferro <James.Bucciferro@PERKINELMER.COM>
2023-03-25 10:01:48 -0400

    End of Section 12

--- client-app/src/app/api/agents.ts
+++ client-app/src/app/api/agents.ts
@@ -59,8 +59,7 @@ const responseBody = <T>(response: AxiosResponse<T>) => response.data;
 
 const requests = {
   get: <T>(url: string) => axios.get<T>(url).then(responseBody),
-  post: <T>(url: string, body: {}) =>
-    axios.post<T>(url, body).then(responseBody),
+  post: <T>(url: string, body: {}) => axios.post<T>(url, body).then(responseBody),
   put: <T>(url: string, body: {}) => axios.put<T>(url, body).then(responseBody),
   del: <T>(url: string) => axios.delete<T>(url).then(responseBody),
 };
@@ -70,8 +69,7 @@ const Activities = {
   list: () => requests.get<Activity[]>("/activities"),
   details: (id: string) => requests.get<Activity>(`/activities/${id}`),
   create: (activity: Activity) => requests.post("/activities", activity),
-  update: (activity: Activity) =>
-    requests.put(`/activities/${activity.id}`, activity),
+  update: (activity: Activity) => requests.put(`/activities/${activity.id}`, activity),
   delete: (id: string) => requests.del(`/activities/${id}`),
 };
 
