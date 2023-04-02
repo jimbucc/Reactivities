6b004a3d24482c9bc5bfdeb679a4a461df15123f
jimbucc <James.Bucciferro@PERKINELMER.COM>
2023-04-02 10:33:18 -0400

    End of Section 20

--- client-app/src/features/activities/details/ActivityDetailedSidebar.tsx
+++ client-app/src/features/activities/details/ActivityDetailedSidebar.tsx
@@ -42,7 +42,11 @@ const ActivityDetailedSidebar = ({ activity: { attendees, host } }: Props) => {
                     {attendee.displayName}
                   </Link>
                 </Item.Header>
-                <Item.Extra style={{ color: "orange" }}>Following</Item.Extra>
+
+                {attendee.following && 
+                  <Item.Extra style={{ color: "orange" }}>Following</Item.Extra>
+                }
+                
               </Item.Content>
             </Item>
           ))}
